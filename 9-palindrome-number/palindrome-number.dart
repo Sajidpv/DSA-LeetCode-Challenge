f3c1f8c5e6612cf5bd238dc3ec205cf8c58ce4a7
SAJID PV <125041012+Sajidpv@users.noreply.github.com>
2025-10-08 10:25:32 +0530

    Time: 2 ms (100.00%) | Memory: 151.6 MB (66.29%) - LeetSync

--- 9-palindrome-number/palindrome-number.dart
+++ 9-palindrome-number/palindrome-number.dart
@@ -1,5 +1,16 @@
 class Solution {
   bool isPalindrome(int x) {
-   return x.toString()==x.toString().split('').reversed.join();
+    if(x<0)return false;
+
+   int rev=0;
+   int temp=x;
+
+   while(temp>0){
+    final digit=temp%10;
+    rev=rev*10+digit;
+  temp=  temp ~/ 10;
+    }
+
+   return rev==x;
   }
 }
