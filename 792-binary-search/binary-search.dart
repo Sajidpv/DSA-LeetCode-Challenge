9693dc0f7e25ccc9fa61b7771a33edfcce179d80
SAJID PV <125041012+Sajidpv@users.noreply.github.com>
2025-11-17 10:22:17 +0530

    Time: 0 ms (100.00%) | Memory: 154.6 MB (86.05%) - LeetSync

--- 792-binary-search/binary-search.dart
+++ 792-binary-search/binary-search.dart
@@ -1,5 +1,15 @@
 class Solution {
   int search(List<int> nums, int target) {
-    return nums.indexOf(target);
+    int start=0;
+    int end=nums.length-1;
+
+    while(start<=end){
+        int mid=start+(end-start)~/2;
+
+        if(nums[mid]==target){return mid;}
+        else if(nums[mid]>target){end=mid-1;}
+        else {start=mid+1;}
+    }
+    return -1;
   }
 }
