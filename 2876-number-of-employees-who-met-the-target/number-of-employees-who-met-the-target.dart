61f5d5aba6fd53ddd7aab982917e44af27d18d6b
SAJID PV <125041012+Sajidpv@users.noreply.github.com>
2025-11-28 17:23:25 +0530

    Time: 1 ms (60.00%) | Memory: 149.1 MB (75.00%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.dart
@@ -0,0 +1,11 @@
+class Solution {
+  int numberOfEmployeesWhoMetTarget(List<int> hours, int target) {
+    int total=0;
+    for (int i=0;i<hours.length;i++){
+        if(hours[i]>=target){
+            total+=1;
+        }
+    }
+    return total;
+  }
+}
